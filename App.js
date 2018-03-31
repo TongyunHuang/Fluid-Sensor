e570d9ee83d25d529c348e963063a4345880499c
gedomel <gedomel2@illinois.edu>
2018-03-31 13:44:45 -0500

    added login screen

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Login from './src/components/Login/Login';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Fluid Sensor App</Text>
-        <Text>Make changes to the App.js file to change what appears here.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <Login/>
+        
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#f4f4f4',
+    flexGrow: 1,
+    //backgroundImage: 'url(map.png)',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
