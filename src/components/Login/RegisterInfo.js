a95f45a1b9bbd03b3612fa3a463b8c5cc325e6b0
gedomel <32102586+gedomel@users.noreply.github.com>
2018-04-02 15:08:56 -0500

    Add files via upload
    Added Registration Elements to Login Screen

---
+++ src/components/Login/RegisterInfo.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TextInput,TouchableOpacity } from 'react-native';
+export default class RegisterInfo extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput 
+                    style= {styles.infoBox}
+                    placeholder="Name"
+                    placeholderTextColor="#36526D"
+                    />
+                <TextInput 
+                    style= {styles.infoBox}
+                    placeholder="Email"
+                    placeholderTextColor="#36526D"
+                    />
+                 <TextInput 
+                    style= {styles.infoBox}
+                    placeholder="Password"
+                    placeholderTextColor="#36526D"
+                    />
+                <TextInput 
+                    style= {styles.infoBox}
+                    placeholder="Weight(lbs)"
+                    placeholderTextColor="#36526D"
+                    />
+                  <TouchableOpacity>
+                      <Text style= {styles.button}>Register</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                      <Text style= {styles.registration}>Have an Account?</Text>
+                  </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding:50,
+        justifyContent:'center',
+    },
+    infoBox:{
+        height:50,
+        backgroundColor:'rgba(151,201,210,0.35)',
+        borderRadius:5,
+        marginTop: 20,
+        
+        padding:16,
+        fontSize:15
+    },
+    button:{
+        fontSize:20,
+        marginTop: 20,
+        marginBottom:40,
+        color:'#36526D',
+        fontWeight:'bold',
+        textAlign:'center'
+    },
+    registration:{
+        fontSize:12,
+        marginTop: 1,
+        marginBottom:14,
+        color:'#36526D',
+        fontWeight:'bold',
+        textAlign:'center'
+    }
+});
